6e1a1ff500c5a669e6c073f90c7e18efa6c603e5
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-03-05 12:26:39 +0100

    corrected typo in URL

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   // console.log(message);
 
   const loadDataFromEndpoint = async (endpoint: `/${string}`) => {
-    const res = await fetch(`http://boiling-river-71009.herokuapp.com${endpoint}`);
+    const res = await fetch(`https://boiling-river-71009.herokuapp.com${endpoint}`);
     const body = await res.json();
     setMessage(body.message);
     setDigipetStats(body.digipet);
